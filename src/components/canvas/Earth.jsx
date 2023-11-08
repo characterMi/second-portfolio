3ff31faf2bbc6a7afd868b6ea3d40d957e245e0c
characterMi <abol1385fx@gmail.com>
2023-11-09 03:23:13 +0330

    set the user scalable to false

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from "../Loader"
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('/second-portfolio/planet/scene.gltf')
   return (
     <primitive
       object={earth.scene}
