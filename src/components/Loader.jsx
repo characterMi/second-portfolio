9de95ecf2c44135ea8918a5c0b3420caee223bf6
characterMi <abol1385fx@gmail.com>
2023-12-08 19:25:46 +0330

    add a loader for models

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,10 @@ const Loader = () => {
   const { progress } = useProgress();
   return (
     <Html>
-      <span className="canvas-load">{progress.toFixed(2)}%</span>
+      <div className="flex text-[14px] text-[#f1f1f1] font-bold mt-[40px] flex-col items-center absolute left-[-2.5rem]">
+        <span className="loader mb-5"></span>
+        {progress.toFixed(2)}%
+      </div>
     </Html>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   ContactShadows,
